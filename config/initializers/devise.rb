2bb091800a8ebb972f146f714bf5439cde8eb6bb
Naveed Ahmad <naveedahmada036@gmail.com>
2024-07-22 14:56:19 +0500

    rollback responder

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -25,10 +25,7 @@ Devise.setup do |config|
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
   require 'devise/orm/active_record'
-
-  config.responder.error_status = :unprocessable_entity
-  config.responder.redirect_status = :see_other
-
+  
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
   # just :email. You can configure it to use [:username, :subdomain], so for

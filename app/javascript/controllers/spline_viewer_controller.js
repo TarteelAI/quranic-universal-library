b199164428d149ec0917b55ba66d43061fbd4a89
Naveed Ahmad <naveedahmada036@gmail.com>
2024-07-23 13:25:39 +0500

    ui improvement

--- app/javascript/controllers/spline_viewer_controller.js
+++ app/javascript/controllers/spline_viewer_controller.js
@@ -12,7 +12,7 @@ export default class extends Controller {
   }
 
   updateViewer() {
-    this.element.querySelectorAll("spline-viewer").forEach((oldViewer) => {
+   /* this.element.querySelectorAll("spline-viewer").forEach((oldViewer) => {
       const newViewer = document.createElement("spline-viewer");
       newViewer.setAttribute("background", "transparent");
       newViewer.setAttribute("loading-anim-type", "spinner-small-light");
@@ -20,31 +20,36 @@ export default class extends Controller {
 
       oldViewer.replaceWith(newViewer);
     });
-
+*/
     setTimeout(()=>{this.adjustCanvasSize()}, 100);
   }
 
   adjustCanvasSize() {
     const canvas = document.querySelector("spline-viewer").shadowRoot.querySelector("canvas");
 
-    const {scale, scaleDesktop, scaleLargeDesktop, marginLeft} = this.element.dataset;
-    if(scale && scale == '1' || !canvas) {
+    const {scale, desktopScale, scaleLargeDesktop, mobileScale, marginLeft} = this.element.dataset;
+    if(scale && scale === '1' || !canvas) {
       return
     }
     const d = this.device;
-    let scaleMobile = 1.5;
 
-    if (d.isDesktop()) {
+    if(d.isMobile() && mobileScale){
+      canvas.style.transform = `scale(${parseFloat(mobileScale)})`;
+      console.log("========= mobile", mobileScale)
+
+    } else if (d.isDesktop()) {
       // Desktop
-      let desktopScale = parseFloat(scaleDesktop || scale || 2.5);
-      canvas.style.transform = `scale(${desktopScale})`;
-      canvas.style.marginLeft = `${marginLeft||'-40px'}`;
+      let s = parseFloat(desktopScale || scale || 1.5);
+      canvas.style.transform = `scale(${s})`;
+      //canvas.style.marginLeft = `${marginLeft||'-40px'}`;
+      console.log("========= desktop", s)
     } else if (d.isLargeDesktop()) {
       // Large Desktop
-      let largeScreeenScale = parseFloat(scaleLargeDesktop || scale || 2.5);
+      let largeScreeenScale = parseFloat(scaleLargeDesktop || scale || 1);
+      console.log("========= largeScreeenScale", largeScreeenScale)
 
       canvas.style.transform = `scale(${largeScreeenScale})`;
-      canvas.style.marginLeft = `-60px`;
+     // canvas.style.marginLeft = `-60px`;
     }
   }
 }

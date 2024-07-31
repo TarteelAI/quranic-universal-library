b69b8dec9474985b41b613e7b2001fe48b50324f
Naveed Ahmad <naveedahmada036@gmail.com>
2024-07-31 15:53:10 +0500

    add translation with inline footnotes

--- lib/exporter/export_translation.rb
+++ lib/exporter/export_translation.rb
@@ -1,46 +1,64 @@
 module Exporter
   class ExportTranslation < BaseExporter
     attr_accessor :resource_content
+    TAG_SANITIZER = Rails::Html::WhiteListSanitizer.new
+    BRIDGRES_FOOTNOTE_MAPPING = {
+      sg: 'Singular',
+      pl: 'Plural',
+      dl: 'Dual'
+    }
 
     def initialize(resource_content:, base_path:)
       super(base_path: base_path, name: resource_content.sqlite_file_name)
       @resource_content = resource_content
     end
 
-    def export_json(ignore_footnotes: false)
-      @json_data = {}
-      json_file_path = "#{export_file_path}.json"
+    def export_json(with_footnotes: false)
+      json_data = {}
 
-      records.find_each do |translation|
-        if ignore_footnotes
-          @json_data[translation.verse_key] =  translation.text
+      if with_footnotes
+        json_file_path = "#{export_file_path}-with-footnote.json"
+      else
+        json_file_path = "#{export_file_path}-simple.json"
+      end
+
+      records.each do |translation|
+        if with_footnotes
+          json_data[translation.verse_key] = translation_text_with_footnotes(translation, chunks: false)
         else
-          @json_data[translation.verse_key] = translation_text_with_footnotes(translation)
+          json_data[translation.verse_key] = translation_text_without_footnotes(translation)
         end
       end
 
       File.open(json_file_path, 'wb') do |file|
-        file << JSON.generate(@json_data, { state: JsonNoEscapeHtmlState.new })
+        file << JSON.generate(json_data, { state: JsonNoEscapeHtmlState.new })
       end
 
       json_file_path
     end
 
-    # Export translations with tags for footnotes
+    # Export translations with html tags(sup) for footnotes
     def export_json_with_footnotes_tags
-      @json_data = {}
+      json_data = {}
       json_file_path = "#{export_file_path}-with-footnote-tags.json"
 
-      records.find_each do |translation|
-        footnotes = {}
-        translation.foot_notes.each do |foot_note|
-          footnotes[foot_note.id] = foot_note.text
-        end
+      records.each do |translation|
+        json_data[translation.verse_key] = translation_text_with_footnotes(translation, chunks: false)
+      end
 
-        @json_data[translation.verse_key] = {
-          t: translation.text.to_s,
-          f: footnotes
-        }
+      File.open(json_file_path, 'wb') do |file|
+        file << JSON.generate(json_data, { state: JsonNoEscapeHtmlState.new })
+      end
+
+      json_file_path
+    end
+
+    def export_json_with_footnotes_chunks
+      json_data = {}
+      json_file_path = "#{export_file_path}-chunks.json"
+
+      records.find_each do |translation|
+        json_data[translation.verse_key] = translation_text_with_footnotes(translation, chunks: true)
       end
 
       File.open(json_file_path, 'wb') do |file|
@@ -50,27 +68,51 @@ module Exporter
       json_file_path
     end
 
-    def export_sqlite
-      if @json_data.blank?
-        export_json
+    def export_sqlite_with_inline_footnotes
+
+    end
+
+    def export_json_with_inline_footnotes
+      json_data = {}
+      json_file_path = "#{export_file_path}-inline-footnotes.json"
+
+      records.map do |translation|
+        json_data[translation.verse_key] = translation_text_with_inline_footnote(translation)
       end
 
-      db_file_path = "#{@export_file_path}.db"
-      statement = create_sqlite_table(db_file_path, 'tafsir', sqlite_db_columns)
+      File.open(json_file_path, 'wb') do |file|
+        file << JSON.generate(json_data, { state: JsonNoEscapeHtmlState.new })
+      end
 
-      @json_data.each do |verse_key, data|
-        fields = [verse_key]
+      json_file_path
+    end
 
-        if data.is_a?(String)
-          group_ayah_key = data
-          fields += [group_ayah_key, '', '', '', '']
-        else
-          group_keys = data[:ayah_keys] || [verse_key]
-          fields += [verse_key, group_keys.first, group_keys.last, group_keys.join(','), data[:text]]
+    def export_sqlite(name: 'translation', with_footnotes: false)
+      if with_footnotes
+        db_file_path = "#{@export_file_path}-with-footnote.db"
+      else
+        db_file_path = "#{@export_file_path}-simple.db"
+      end
+
+      statement = create_sqlite_table(db_file_path, name, translation_table_columns(with_footnotes: with_footnotes))
+
+      records.each do |record|
+        text = with_footnotes ? translation_text_with_footnotes(record, chunks: false) : translation_text_without_footnotes(record)
+
+        fields = [
+          record.chapter_id,
+          record.verse_number,
+          record.verse_key,
+          text[:t]
+        ]
+
+        if with_footnotes
+          fields << JSON.generate(text[:f])
         end
 
         statement.execute(fields)
       end
+
       close_sqlite_table
 
       db_file_path
@@ -78,59 +120,122 @@ module Exporter
 
     protected
 
-    def translation_text_with_footnotes(translation)
-      text = translation&.text
-      footnotes_refs = {}
-      footnotes = {}
+    def translation_text_with_footnotes(translation, chunks: false)
+      text = translation.text.to_s
 
-      if text.blank? || (!text.include?('<sup') && !translation.resource_content_id == 149)
-        result = {
-          t: [text.to_s.strip]
-        }
+      # brides translation have hard coded footnotes, and needs a custom export
+      if text.blank?
+        if chunks
+          {
+            t: [text.to_s.strip],
+            f: []
+          }
+        else
+          {
+            t: text.to_s.strip,
+            f: []
+          }
+        end
       else
         doc = Nokogiri::HTML::DocumentFragment.parse(text)
-        if translation.resource_content_id == 149
-          result = export_bridres_with_footnote(doc)
+        # bridges translation
+        if is_bridges_translation? && chunks
+          export_bridres_with_footnote(doc)
         else
-          foot_note_counter = 1
-          doc.children.each do |node|
-            if node.name == 'text'
-              next
-            end
-
-            id = node.attr('foot_note')
-            if id.present? && (foot_note = FootNote.where(id: id).first).present?
-              # Some footnote also has html tags tags, strip those tags
-              foot_note_text = Nokogiri::HTML::DocumentFragment.parse(foot_note.text).text
-              stripped = foot_note_text.tr(" ", '').strip
-
-              footnotes[foot_note_counter] = stripped
-              footnotes_refs[id] = foot_note_counter
-              foot_note_counter += 1
-            end
+          if chunks
+            export_translation_chunks(doc)
+          else
+            export_simple_translation(doc, translation)
           end
+        end
+      end
+    end
 
-          translation_chunks = []
-          doc.children.each do |child|
-            id = child.attr('foot_note')
+    def export_translation_chunks(doc)
+      footnotes_refs = {}
+      footnotes = {}
+      foot_note_counter = 1
+      doc.children.each do |node|
+        if node.name == 'text'
+          next
+        end
 
-            if id.present?
-              translation_chunks << {
-                f: footnotes_refs[id]
-              }
-            else
-              translation_chunks << child.text if child.text.presence.present?
-            end
-          end
+        id = node.attr('foot_note')
+        if id.present? && (foot_note = FootNote.where(id: id).first).present?
+          # Some footnote also has html tags tags, strip those tags
+          foot_note_text = Nokogiri::HTML::DocumentFragment.parse(foot_note.text).text
+          stripped = foot_note_text.tr(" ", '').strip
 
-          result = {
-            t: translation_chunks,
-            f: footnotes
-          }
+          footnotes[foot_note_counter] = stripped
+          footnotes_refs[id] = foot_note_counter
+          foot_note_counter += 1
         end
       end
 
-      result
+      translation_chunks = []
+      doc.children.each do |child|
+        id = child.attr('foot_note')
+
+        if id.present?
+          translation_chunks << {
+            f: footnotes_refs[id]
+          }
+        else
+          translation_chunks << child.text if child.text.presence.present?
+        end
+      end
+
+      {
+        t: translation_chunks,
+        f: footnotes
+      }
+    end
+
+    def export_simple_translation(doc, translation)
+      footnotes = {}
+      translation.foot_notes.each do |foot_note|
+        footnotes[foot_note.id] = foot_note.text
+      end
+
+      {
+        t: doc.to_s.strip,
+        f: footnotes
+      }
+    end
+
+    def translation_text_without_footnotes(translation)
+      text = translation.text
+      doc = Nokogiri::HTML::DocumentFragment.parse(text)
+      doc.search('a').remove
+      doc.search('sup').remove
+
+      {
+        t: doc.text.strip
+      }
+    end
+
+    def translation_text_with_inline_footnote(translation)
+      text = translation.text.to_s
+
+      doc = Nokogiri::HTML::DocumentFragment.parse(text)
+
+      doc.search("a sup, sup").each do |node|
+        t = node.text.strip.to_sym
+        footnote_id = node.attr('foot_note')
+
+        if footnote = BRIDGRES_FOOTNOTE_MAPPING[t]
+          node.content = "[[#{footnote}]]"
+        elsif footnote_id.present?
+          footnote = FootNote.find_by_id(footnote_id)
+          if footnote.present?
+            node.content = "[[#{footnote.text}]]"
+          else
+            node.remove
+          end
+        end
+      end
+
+      { t: doc.text }
     end
 
     def export_bridres_with_footnote(doc)
@@ -176,20 +281,37 @@ module Exporter
       }
     end
 
-
-    def sqlite_db_columns
-      {
+    def translation_table_columns(with_footnotes: false)
+      cols = {
+        sura: 'INTEGER',
+        ayah: 'INTEGER',
         ayah_key: 'TEXT',
-        group_ayah_key: 'TEXT',
-        from_ayah: 'TEXT',
-        to_ayah: 'TEXT',
-        ayah_keys: 'TEXT',
         text: 'TEXT',
+      }
+
+      if with_footnotes
+        cols[:footnotes] = 'TEXT'
+      end
+
+      cols
+    end
+
+    def table_column_names
+      {
+        chapter_id: 'surah_number',
+        verse_id: 'ayah_number',
+        verse_key: 'ayah_key',
+        footnotes: 'footnotes',
+        text: 'text'
       }
     end
 
     def records
       Translation.where(resource_content_id: resource_content.id).order('verse_id ASC')
+    end
+
+    def is_bridges_translation?
+      149 == resource_content.id
     end
   end
 end

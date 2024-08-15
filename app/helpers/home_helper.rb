0de64a55df8c264192f0481651556118e92409e3
Naveed Ahmad <naveedahmada036@gmail.com>
2024-08-15 20:22:38 +0500

    refactor tools ui

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -57,14 +57,6 @@ module HomeHelper
   end
 
   def diff_text(text1, text2)
-    # diff = `git diff $(echo "#{text1}" | git hash-object -w --stdin) $(echo "#{text2}" | git hash-object -w --stdin)  --word-diff`
-
-    # if diff.present?
-    #  result = diff.split('@@').last.strip
-
-    #  [result, result.gsub(/\[-/, ' <del> ').gsub(/-\]/, ' </del> ').gsub(/\{\+/, ' <ins> ').gsub(/\+\}/, ' </ins> ')].join("</br></br/>").html_safe
-    #end
-
     Diffy::SplitDiff.new(text1, text2, format: :html).right.html_safe
   end
 end

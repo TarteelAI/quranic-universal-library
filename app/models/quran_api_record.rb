8fd3cfdeef5a542ffcfe5e1ffe840d036854616a
Naveed Ahmad <naveedahmada036@gmail.com>
2024-10-23 05:00:22 +0500

    add word images

--- app/models/quran_api_record.rb
+++ app/models/quran_api_record.rb
@@ -2,10 +2,9 @@ class QuranApiRecord < ApplicationRecord
   self.abstract_class = true
   self.establish_connection Rails.env.development? ? :quran_api_db_dev : :quran_api_db
 
-  CDN_HOST = 'https://static.qurancdn.com'
-  WORDS_CDN = 'https://static-cdn.tarteel.ai/qul/images/w'
-  AYAH_CDN = 'https://static-cdn.tarteel.ai/qul/images/ayah'
-
+  CDN_HOST = 'https://static-cdn.tarteel.ai'
+  WORDS_CDN = "#{CDN_HOST}/qul/images/w"
+  AYAH_CDN = "#{CDN_HOST}/qul/images/ayah"
 
   def toggle_approve!
     update_attribute :approved, !self.approved?

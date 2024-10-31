dffc0e4cc6273c4044f75868521bd8ff9bf9c610
Naveed Ahmad <naveedahmada036@gmail.com>
2024-10-31 05:47:45 +0500

    Add export script

---
+++ app/controllers/exports_controller.rb
@@ -0,0 +1,40 @@
+class ExportsController < ApplicationController
+  layout 'export'
+  before_action :set_defaults
+
+  def word
+    @word = if params[:word].include?(':')
+              Word.find_by(location: params[:word])
+            else
+              Word.find_by(word_index: params[:word])
+            end
+  end
+
+  def ayah
+    @verse = if params[:ayah].include?(':')
+               Verse.find_by(verse_key: params[:ayah])
+            else
+              Verse.find_by(verse_index: params[:ayah])
+            end
+  end
+
+  def mushaf_page
+    @mushaf = Mushaf.find(params[:mushaf_id])
+
+    @words = MushafWord.where(
+      mushaf_id: params[:mushaf_id],
+      page_number: params[:page_number]
+    ).order('position_in_page ASC')
+  end
+
+  protected
+
+  def set_defaults
+    params[:word] ||= '1:1:1'
+    params[:ayah] ||= '1:1'
+    params[:script] ||= 'code_v1'
+    params[:font_size] ||= '36'
+    params[:mushaf_id] ||= 1
+    params[:page_number] ||= 1
+  end
+end

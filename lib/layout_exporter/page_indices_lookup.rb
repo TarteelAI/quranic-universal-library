5058f45c1f25760c3c4237d1eab66bc05942ad7f
Naveed Ahmad <naveedahmada036@gmail.com>
2024-12-28 12:58:22 +0500

    layout data export

---
+++ lib/layout_exporter/page_indices_lookup.rb
@@ -0,0 +1,38 @@
+module LayoutExporter
+  class PageIndicesLookup < Base
+    def export(rub: false, manzil: false)
+      folder = get_mushaf_file_name
+      export_path = "tmp/page-indices-lookup"
+      FileUtils.mkdir_p export_path
+      pages = []
+
+      MushafPage.where(mushaf: mushaf).order('page_number ASC').each do |page|
+        first_ayah = page.first_verse
+        last_ayah = page.last_verse
+
+        page_mapping = {
+          juz: first_ayah.juz_number,
+          hizb: first_ayah.hizb_number,
+          rub: rub && first_ayah.rub_el_hizb_number,
+          manzil: manzil && first_ayah.manzil_number,
+          start: {
+            surah: first_ayah.chapter_id,
+            ayah: first_ayah.verse_number
+          },
+          end: {
+            surah: last_ayah.chapter_id,
+            ayah: last_ayah.verse_number
+          }
+        }
+
+        pages << page_mapping.compact_blank
+      end
+
+      File.open("#{export_path}/#{folder}.json", "wb") do |f|
+        f << JSON.generate(pages, { state: JsonNoEscapeHtmlState.new })
+      end
+
+      "#{export_path}/#{folder}.json"
+    end
+  end
+end

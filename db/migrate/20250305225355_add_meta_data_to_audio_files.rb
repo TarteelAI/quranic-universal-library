62eeca5f7fa30176c76c5f9573e3a49751a29a03
Naveed Ahmad <naveedahmada036@gmail.com>
2025-03-13 17:13:33 +0500

    add more fields for audio files to track meta data

---
+++ db/migrate/20250305225355_add_meta_data_to_audio_files.rb
@@ -0,0 +1,22 @@
+class AddMetaDataToAudioFiles < ActiveRecord::Migration[7.0]
+  def change
+    c = AudioFile.connection
+    c.add_column :recitations, :relative_path, :string, if_not_exists: true
+    c.add_column :recitations, :name, :string, if_not_exists: true
+
+    c.add_column :audio_files, :bit_rate, :float
+    c.add_column :audio_files, :duration_ms, :integer
+    c.add_column :audio_files, :meta_data, :jsonb, default: {}
+    c.add_column :audio_files, :file_size, :integer
+    c.add_column :audio_files, :has_repetition, :boolean, default: false
+    c.add_column :audio_files, :repeated_segments, :string
+
+    c.change_column :audio_files, :duration, :float
+    c.add_column :audio_chapter_audio_files, :segments_count, :integer, default: 0
+
+    c.add_column :audio_segments, :repeated_segments, :string
+    c.add_column :audio_segments, :has_repetition, :boolean, default: false
+    c.add_index :audio_segments, :has_repetition
+    c.add_index :audio_files, :has_repetition
+  end
+end

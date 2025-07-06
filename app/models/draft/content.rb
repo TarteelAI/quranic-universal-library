e69edf4280bd0a598a362e6bea215cdfca9ce741
Naveed Ahmad <naveedahmada036@gmail.com>
2025-07-07 03:33:21 +0500

    Export search db (#319)
    * upgrade sqlite
    
    * divide surah and ayah FTS tables

--- app/models/draft/content.rb
+++ app/models/draft/content.rb
@@ -34,4 +34,30 @@ class Draft::Content < ApplicationRecord
   belongs_to :verse
   belongs_to :chapter
   belongs_to :resource_content
+
+  def self.draft_resources
+    counts = Draft::Content
+               .group(:resource_content_id)
+               .select("resource_content_id,
+             COUNT(*) AS total_count,
+             COUNT(CASE WHEN text_matched = true THEN 1 END) AS matched_count,
+             COUNT(CASE WHEN text_matched = false THEN 1 END) AS not_matched_count,
+             COUNT(CASE WHEN imported = true THEN 1 END) AS imported_count,
+             COUNT(CASE WHEN imported = false THEN 1 END) AS not_imported_count,
+             COUNT(CASE WHEN need_review = true THEN 1 END) AS need_review_count")
+
+    resources = ResourceContent.where(id: counts.map(&:resource_content_id)).index_by(&:id)
+
+    counts.map do |record|
+      {
+        resource: resources[record.resource_content_id],
+        total_count: record.total_count.to_i,
+        matched_count: record.matched_count.to_i,
+        not_matched_count: record.not_matched_count.to_i,
+        imported_count: record.imported_count.to_i,
+        not_imported_count: record.not_imported_count.to_i,
+        need_review_count: record.need_review_count.to_i
+      }
+    end
+  end
 end
---
+++ lib/export_quran_fts.rb
@@ -0,0 +1,159 @@
+require "sqlite3"
+
+class ExportQuranFts
+  DB_PATH = Rails.root.join("tmp", "search-data.sqlite")
+
+  def self.run
+    new.export
+  end
+
+  def export
+    File.delete(DB_PATH) if File.exist?(DB_PATH)
+
+    db = SQLite3::Database.new(DB_PATH.to_s)
+    db.results_as_hash = true
+
+    create_tables(db)
+
+    insert_surahs(db)
+    insert_ayahs(db)
+    optimize_table(db)
+    db.close
+    puts "FTS export complete: #{DB_PATH}"
+  end
+
+  private
+
+  def create_tables(db)
+    db.execute_batch <<~SQL
+      CREATE VIRTUAL TABLE surah_index USING fts5(
+        term,
+        key UNINDEXED,
+        tokenize = 'unicode61 remove_diacritics 1'
+      );
+
+      CREATE VIRTUAL TABLE ayah_index USING fts5(
+        term,
+        key UNINDEXED,
+        tokenize = 'trigram'
+      );
+    SQL
+  end
+
+  protected
+
+  def insert_surahs(db)
+    Chapter.find_each do |chapter|
+      terms = [
+        chapter.name_simple.downcase,
+        chapter.name_arabic,
+        chapter.name_complex.downcase,
+        chapter.id.to_s
+      ]
+
+      chapter.navigation_search_records.each do |search|
+        term = search.text.downcase
+        terms << term
+
+        if term.include?('al ') || term.include?('an ')
+          terms << term.sub(/^(al|an) /, '\1').strip
+        end
+      end
+
+      terms = terms.map { |term| normalize(term) }
+
+      terms.uniq.each do |term|
+        db.execute <<~SQL, [term, chapter.id.to_s]
+          INSERT INTO surah_index (term, key)
+          VALUES (?, ?);
+        SQL
+      end
+    end
+  end
+
+  def insert_ayahs(db)
+    Verse.includes(:verse_root, :verse_stem, :verse_lemma).find_each do |verse|
+      transliterations = Translation.where(
+        resource_content_id: [1561, 57, 1566],
+        verse_id: verse.id
+      )
+
+      terms = [
+        normalize_arabic(verse.text_uthmani),
+        normalize_arabic(verse.text_uthmani_simple),
+        normalize_arabic(verse.text_imlaei),
+        normalize_arabic(verse.text_qpc_hafs.gsub(/[٠١٢٣٤٥٦٧٨٩]/, '')),
+        normalize_arabic(verse.text_imlaei_simple),
+        normalize_arabic(verse.verse_stem&.text_madani),
+        normalize_arabic(verse.verse_lemma&.text_madani),
+        normalize_arabic(verse.verse_root&.value),
+      ].compact_blank
+
+      transliterations.each do |tr|
+        terms << normalize(tr.text)
+      end
+
+      terms.uniq.each do |term|
+        db.execute <<~SQL, [term, verse.verse_key]
+          INSERT INTO ayah_index (term, key)
+          VALUES (?, ?);
+        SQL
+      end
+    end
+  end
+
+  def optimize_table(db)
+    db.execute "INSERT INTO surah_index(term) VALUES('optimize');"
+    db.execute "INSERT INTO ayah_index(term) VALUES('optimize');"
+  end
+
+  HAFS_WAQF_FOR_PHRASE = ["ۖ", "ۗ", "ۚ", "ۚ", "ۜ", "ۢ", "ۨ", "ۭ"]
+  HAFS_WAQF_WITH_SIGNS = ["ـ", "ۖ", "ۗ", "ۘ", "ۚ", "ۛ", "ۜ", "۞", "ۢ", "ۦ", "ۧ", "ۨ", "۩", "۪", "۬", "ۭ"]
+  INDOPAK_WAQF = ["ۛ", "ٚ", "ؔ", "ؕ", "ۥ", "ۚ", "۪", "۠", "ۙ", "ؗ", "۫", "ۘ", "ۗ", "۬", "ۙ", "۬", "ۦ"]
+  EXTRA_CHARS = ['', '', '', '', '‏', ',', '‏', '​', '', '‏', "\u200f"]
+  WAQF_REG = Regexp.new((HAFS_WAQF_WITH_SIGNS + INDOPAK_WAQF + EXTRA_CHARS).join('|'))
+
+  TASHKEEL_MATCHERS = [
+    [/[آٱأإ]/, 'ا'],
+    [/[ٰ]/, 'ا'],
+    [/[ؤئ]/, 'ء'],
+    [/ة/, 'ه'],
+    [/ى/, 'ي'],
+    [/[ًٌٍَُِّْـ]/, ''],
+    [/۪/, ''],
+    [/۫/, ''],
+    [/[،؛؟؞.!]/, '']
+  ]
+
+  def normalize_arabic(text)
+    return if text.blank?
+
+    TASHKEEL_MATCHERS.each do |pattern, replacement|
+      text = text.gsub(pattern, replacement)
+    end
+
+    remove_diacritics(text)
+  end
+
+  def remove_diacritics(text)
+    return if text.to_s.presence.blank?
+
+    text = text.to_s.remove_diacritics
+    text.gsub(WAQF_REG, '')
+        .strip
+  end
+
+  def normalize(text)
+    text = text.downcase
+    text = text.gsub(/\p{Mn}/, '')
+    t = I18n.transliterate(text)
+    text = t unless t.include?('?')
+    text.gsub(/[-]|['’]|\b(suresi|surasi|surat|surah|sura|chapter|سورہ|سورت|سورة)\b/, ' ')
+        .gsub(/\s+/, ' ').strip
+  end
+
+  def normalize_words(text)
+    return if text.to_s.presence.blank?
+    remove_diacritics(text)
+  end
+end

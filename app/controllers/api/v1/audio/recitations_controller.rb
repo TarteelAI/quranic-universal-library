b12601f3d5414dce39c4f164cbbb43b29c565e21
Naveed Ahmad <naveedahmada036@gmail.com>
2025-07-22 16:42:29 +0500

    Add recitaiton api (#338)

--- app/controllers/api/v1/audio/recitations_controller.rb
+++ app/controllers/api/v1/audio/recitations_controller.rb
@@ -2,8 +2,10 @@ module Api::V1
   module Audio
     class RecitationsController < ApiController
       def surah_recitations; end
+      def surah_recitation_detail; end
 
       def ayah_recitations; end
+      def ayah_recitation_detail; end
 
       protected
 
--- app/presenters/v1/audio/recitation_presenter.rb
+++ app/presenters/v1/audio/recitation_presenter.rb
@@ -12,6 +12,14 @@ module V1
           .approved
           .includes(:recitation_style, :qirat_type)
       end
+
+      def surah_recitation
+        surah_recitations.find(params[:id])
+      end
+
+      def ayah_recitation
+        ayah_recitations.find(params[:id])
+      end
     end
   end
 end

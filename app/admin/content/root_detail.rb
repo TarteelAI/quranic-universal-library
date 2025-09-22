737e041aaac70c5590458f4e65580f2212c04b82
MuhammadAbdullaah <mabdullahsaeed188@gmail.com>
2025-09-22 15:14:56 +0500

    fix for nil root_id

--- app/admin/content/root_detail.rb
+++ app/admin/content/root_detail.rb
@@ -5,10 +5,14 @@ ActiveAdmin.register RootDetail do
   actions :all, except: :destroy
   ActiveAdminViewHelpers.versionate(self)
 
-  permit_params do
-    %i[root_id language_id language_name resource_content_id root_detail meta_data]
-  end
+  permit_params :token_id,
+                :root_id,
+                :language_id,
+                :resource_content_id,
+                :text,
+                :meta_data
 
+  filter :token_id
   filter :root_id
   filter :language,
          as: :searchable_select,
@@ -16,23 +20,36 @@ ActiveAdmin.register RootDetail do
   filter :resource_content,
          as: :searchable_select,
          ajax: { resource: ResourceContent }
-  filter :root_detail
+  filter :text
   filter :created_at
   filter :updated_at
 
   index do
     id_column
+
+    column :token_id do |resource|
+      if resource.respond_to?(:token) && resource.token
+        link_to resource.token_id, [:cms, resource.token]
+      else
+        resource.token_id
+      end
+    end
+
     column :root_id do |resource|
       link_to resource.root_id, [:cms, resource.root] if resource.root
     end
-    column :language_name
-    column :resource_content do |resource|
-      rc = resource.resource_content
-      link_to(rc.name, [:cms, rc]) if rc
+
+    column :language do |resource|
+      resource.language&.name
     end
 
-    column :root_detail do |resource|
-      text = resource.root_detail.to_s
+    column :resource_content do |resource|
+      rc = resource.resource_content
+      link_to rc.name, [:cms, rc] if rc
+    end
+
+    column :text do |resource|
+      text = resource.text.to_s
       preview = ActionController::Base.helpers.strip_tags(text).squish.truncate(140)
       span preview
     end
@@ -45,19 +62,32 @@ ActiveAdmin.register RootDetail do
   show do
     attributes_table do
       row :id
+
+      row :token_id do
+        if resource.respond_to?(:token) && resource.token
+          link_to resource.token_id, [:cms, resource.token]
+        else
+          resource.token_id
+        end
+      end
+
       row :root_id do
         link_to resource.root_id, [:cms, resource.root] if resource.root
       end
-      row :language_id
-      row :language_name
+
+      row :language do
+        resource.language&.name
+      end
+
       row :resource_content do
         r = resource.resource_content
         link_to r.name, [:cms, r] if r
       end
 
-      row :root_detail do
-        div class: resource.language_name.to_s.downcase do
-          safe_html resource.root_detail
+      row :text do
+        css_class = resource.language&.name.to_s.downcase
+        div class: css_class do
+          safe_html resource.text
         end
       end
 
@@ -76,15 +106,15 @@ ActiveAdmin.register RootDetail do
 
   form do |f|
     f.inputs 'Root Detail' do
+      f.input :token_id
       f.input :root_id
-      f.input :root_detail, as: :text
+      f.input :text, as: :text
       f.input :resource_content,
               as: :searchable_select,
               ajax: { resource: ResourceContent }
       f.input :language,
               as: :searchable_select,
               ajax: { resource: Language }
-      f.input :language_name
     end
 
     f.actions
--- db/migrate/20250916073555_create_root_details.rb
+++ db/migrate/20250916073555_create_root_details.rb
@@ -1,12 +1,14 @@
 class CreateRootDetails < ActiveRecord::Migration[7.0]
   def change
     c = Verse.connection
+    c.drop_table :root_details, if_exists: true
+
     c.create_table :root_details do |t|
-      t.integer :root_id,              index: true, null: false
+      t.integer :token_id,             index: true
+      t.integer :root_id,              index: true
       t.integer :language_id,          index: true
-      t.string  :language_name,        index: true
       t.integer :resource_content_id,  index: true
-      t.text    :root_detail
+      t.text    :text
 
       t.jsonb   :meta_data,           default: {}, null: false
 

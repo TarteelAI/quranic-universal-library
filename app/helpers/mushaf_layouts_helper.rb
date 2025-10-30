18ed2c678a4b376e004a8daf7e6dbc97a6ae0be5
nblasd <gameranonymous338@gmail.com>
2025-10-30 15:06:59 +0500

    Add help icon, sorting, and loading indicator to mushaf page mapping view
    Features added:
    - Help icon with tooltip explaining ayah key format (surah:ayah)
    - Sortable page column (first click: descending, second: ascending)
    - Loading spinner at top of table during sorting
    - Changed info-tip icon color from white to black
    
    Technical changes:
    - Created MushafLayoutsHelper with sort_order_link method
    - Created turbo_frame_loading_controller.js for loading states
    - Registered new Stimulus controller in index.js
    - Updated page_mapping partial with help icon and loading indicator
    - Updated info-tip partial with black icon color

---
+++ app/helpers/mushaf_layouts_helper.rb
@@ -0,0 +1,26 @@
+module MushafLayoutsHelper
+  def sort_order_link(text, sort_key, url_params = {}, link_options = {})
+    current_order = params[:sort_order] || 'asc'
+    current_key = params[:sort_key]
+    
+    if current_key == sort_key.to_s
+      order = current_order == 'asc' ? 'desc' : 'asc'
+    else
+      order = 'desc'
+    end
+
+    icon_asc = "<i class='fa fa-sort-up #{'active' if current_order == 'asc' && current_key == sort_key.to_s}'></i>"
+    icon_desc = "<i class='fa fa-sort-down #{'active' if current_order == 'desc' && current_key == sort_key.to_s}'></i>"
+
+    url_params.merge!(
+      sort_key: sort_key,
+      sort_order: order
+    )
+
+    link_options[:class] = "d-flex sort-link #{link_options[:class]}"
+
+    link_to mushaf_layout_path(url_params), link_options do
+      "<span class='label-text tw-me-2'>#{text}</span> <span class='sort-icons'>#{icon_asc} #{icon_desc}</span>".html_safe
+    end
+  end
+end

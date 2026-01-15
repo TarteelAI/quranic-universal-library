651dfdc50a86df09d8a4855bc6cce89b01316c4f
nblasd <gameranonymous338@gmail.com>
2026-01-15 17:57:32 +0500

    Refactor login failure to proper Turbo response

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,14 +1,28 @@
 class Users::SessionsController < Devise::SessionsController
   def create
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message!(:notice, :signed_in)
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
-  rescue HTML5::ParseError, ActionController::InvalidAuthenticityToken, ActionController::ParameterMissing, Devise::Strategies::DatabaseAuthenticatable::Error
-    # This is a bit simplified, but Devise usually throws :warden on failure
-    # and redirects to failure app. For Turbo we want to stay on the same page and show errors.
-    render_error_response
+    self.resource = resource_class.find_for_database_authentication(email: sign_in_params[:email])
+
+    if resource&.valid_password?(sign_in_params[:password])
+      if resource.active_for_authentication?
+        resource.remember_me = sign_in_params[:remember_me] if resource.respond_to?(:remember_me=)
+        set_flash_message!(:notice, :signed_in)
+        sign_in(resource_name, resource)
+        yield resource if block_given?
+        respond_with resource, location: after_sign_in_path_for(resource)
+      else
+        set_flash_message!(:notice, :"signed_in_but_#{resource.inactive_message}")
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_in_path_for(resource)
+      end
+    else
+      self.resource = resource_class.new(sign_in_params)
+      clean_up_passwords(resource)
+      flash.now[:alert] = I18n.t("devise.failure.invalid", authentication_keys: resource_class.authentication_keys.join(", "))
+      respond_to do |format|
+        format.html { render :new, status: :unprocessable_entity }
+        format.turbo_stream { render :new, status: :unprocessable_entity }
+      end
+    end
   end
 
   protected

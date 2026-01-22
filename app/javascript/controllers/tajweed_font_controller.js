7e7a6477acb7abdd220e7f0ddd58137876128bca
Naveed Ahmad <naveedahmada036@gmail.com>
2026-01-23 00:12:35 +0500

    Fix Font color palette

--- app/javascript/controllers/tajweed_font_controller.js
+++ app/javascript/controllers/tajweed_font_controller.js
@@ -8,21 +8,26 @@ export default class extends Controller {
   }
 
   addThemeButtons() {
-    const light = "<button class='btn btn-sm btn-primary' data-theme='theme-light'>Light</button>"
-    const dark = "<button class='btn btn-sm btn-primary' data-theme='theme-dark'>Dark</button>"
-    const sepia = "<button class='btn btn-sm btn-primary' data-theme='theme-sepia'>Sepia</button>"
-    const black = "<button class='btn btn-sm btn-primary' data-theme='theme-black-text'>Black</button>"
-    const light2 = "<button class='btn btn-sm btn-primary' data-theme='theme-light2'>L2</button>"
-    const dark2 = "<button class='btn btn-sm btn-primary' data-theme='theme-dark2'>D2</button>"
-    const sepia2 = "<button class='btn btn-sm btn-primary' data-theme='theme-sepia2'>S2</button>"
+    const light = "<button class='tw-btn tw-btn-sm tw-btn-info' data-theme='theme-light'>Light</button>"
+    const dark = "<button class='tw-btn tw-btn-sm tw-btn-info' data-theme='theme-dark'>Dark</button>"
+    const sepia = "<button class='tw-btn tw-btn-sm tw-btn-info' data-theme='theme-sepia'>Sepia</button>"
+    const black = "<button class='tw-btn tw-btn-sm tw-btn-info' data-theme='theme-black'>Black</button>"
 
-    this.el.append(`<div>${light} ${light2} ${dark} ${dark2} ${sepia} ${sepia2} ${black}</div>`)
+    const p1 = "<button class='tw-btn tw-btn-sm tw-btn-info' data-theme='theme1'>P1</button>"
+    const p2 = "<button class='tw-btn tw-btn-sm tw-btn-info' data-theme='theme2'>P2</button>"
+    const p3 = "<button class='tw-btn tw-btn-sm tw-btn-info' data-theme='theme3'>P3</button>"
+    const p4 = "<button class='tw-btn tw-btn-sm tw-btn-info' data-theme='theme4'>P4</button>"
+    const p5 = "<button class='tw-btn tw-btn-sm tw-btn-info' data-theme='theme5'>P5</button>"
+    const p6 = "<button class='tw-btn tw-btn-sm tw-btn-info' data-theme='theme6'>P6</button>"
+
+    this.el.append(`<div>${light} ${dark} ${sepia} ${black} ${p1} ${p2} ${p3} ${p4} ${p5} ${p6}</div>`)
   }
 
   bindEvents(){
     this.el.find('[data-theme]').on('click', (e) => {
-      this.el.removeClass("theme-light theme-dark theme-sepia theme-light2 theme-dark2 theme-sepia2 theme-black-text")
-      this.el.addClass(e.target.dataset.theme)
+      const theme = e.target.dataset.theme;
+      this.el.removeClass("theme-light theme-dark theme-sepia theme-black theme-normal theme1 theme2 theme3 theme4 theme5 theme6")
+      this.el.addClass(theme);
     })
   }
 }

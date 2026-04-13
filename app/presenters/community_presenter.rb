738eda2524d3e3f7f180625725006baa9b87cb73
nblasd <gameranonymous338@gmail.com>
2026-04-13 12:42:48 +0500

    Enhance meta_title method in CommunityPresenter for dynamic page titles based on action name

--- app/presenters/community_presenter.rb
+++ app/presenters/community_presenter.rb
@@ -1,6 +1,15 @@
 class CommunityPresenter < ApplicationPresenter
   def meta_title
-    "QUL Tools"
+    case action_name
+    when 'tools'
+      "QUL Tools"
+    when 'faq'
+      "FAQ"
+    when 'credits'
+      "Credits"
+    else
+      "QUL Tools"
+    end
   end
 
   def meta_description

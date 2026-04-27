1c3505637b6417c3f7bed80ce835bab1a313546c
Naveed Ahmad <naveedahmada036@gmail.com>
2026-04-28 00:24:14 +0500

    Share change log with resource update email (#594)
    * Share change log with resource update email
    
    * fix url

--- app/mailers/downloadable_resource_mailer.rb
+++ app/mailers/downloadable_resource_mailer.rb
@@ -5,10 +5,12 @@ class DownloadableResourceMailer < ApplicationMailer
   #
   #   en.downloadable_resource_mailer.new_update.subject
   #
-  def new_update(resource, user)
+  def new_update(resource, user, change_log = nil)
     @user = user
     @resource = resource
-    @subject = "New Update Available for Your Downloaded Resource(#{resource.name})"
+    @change_log = change_log
+    @resource_type = resource.group_name.presence || resource.resource_type.to_s.tr('-', ' ').titleize
+    @subject = "Updated #{@resource_type} Available: #{resource.name}"
 
     mail to: user.email, subject: @subject
   end
